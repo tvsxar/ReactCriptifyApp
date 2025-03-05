c4bb162031fe5c57dce7c104a13948f719524740
Taras Poiatsyka <tvsxar@MacBook-Air---Taras.local>
2025-03-06 00:20:19 +0200

    button fixed

--- src/components/Generator/Generator.tsx
+++ src/components/Generator/Generator.tsx
@@ -1,5 +1,5 @@
 import './Generator.scss';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import ConditionButton from '../ConditionButton/ConditionButton';
 
 // images
@@ -11,22 +11,20 @@ type buttonType = 'uppercase' | 'lowercase' | 'special' | 'numbers';
 function Generator() {
     const [length, setLength] = useState<number>();
     const [password, setPassword] = useState<string>('');
+    const [copyMessage, setCopyMessage] = useState<string>('Copied');
+    const [showCopyMessage, setShowCopyMessage] = useState<boolean>(false);
 
     const [includeUppercase, setIncludeUppercase] = useState<boolean>(true);
     const [includeLowercase, setIncludeLowercase] = useState<boolean>(true);
     const [includeSpecial, setIncludeSpecial] = useState<boolean>(true);
     const [includeNumbers, setIncludeNumbers] = useState<boolean>(true);
 
+    const isCopying = useRef(false);
+
     async function generatePassword(e : React.FormEvent) {
         e.preventDefault();
 
         const url : string = `https://api.genratr.com/?length=${length}${includeUppercase ? '&uppercase' : ''}${includeLowercase ? '&lowercase' : ''}${includeSpecial ? '&special' : ''}${includeNumbers ? '&numbers' : ''}`;
-
-        console.log(url);
-        console.log(includeUppercase);
-        console.log(includeLowercase);
-        console.log(includeSpecial);
-        console.log(includeNumbers);
 
         try {
             const response = await fetch(url);
@@ -37,12 +35,27 @@ function Generator() {
 
             const data = await response.json();
             setPassword(data.password);
+            setLength(12);
         } catch(error) {
             console.error('Error generating password:', error);
         }
     }
 
-    function handleButtonClick(type : buttonType) {
+    function handleConditionButtonClick(type : buttonType) {
+        // if only one condition is active, don't allow to deactivate it
+        const states = {
+            lowercase: includeLowercase,
+            uppercase: includeUppercase,
+            numbers: includeNumbers,
+            special: includeSpecial,
+        };
+
+        const activeCount = Object.values(states).filter(state => state).length;
+
+        if(activeCount === 1 && states[type]) return;
+
+        // toggle the condition
+
         switch(type) {
             case 'lowercase':
                 setIncludeLowercase(prev => !prev);
@@ -59,6 +72,31 @@ function Generator() {
         }
     }
 
+    function handleCopyButtonClick(e : React.FormEvent) {
+        e.preventDefault();
+
+        if (isCopying.current) {
+            return; // if true - return
+        }
+
+        isCopying.current = true;
+
+        // copy the password to the clipboard if password is true
+        if(password) {
+            navigator.clipboard.writeText(password);
+            setCopyMessage('Copied')
+        } else {
+            setCopyMessage('Nothing to copy')
+        }
+       
+        // show the message
+        setShowCopyMessage(prev => !prev);
+        setTimeout(() => {
+            setShowCopyMessage(prev => !prev) // hide message after 2 seconds
+            isCopying.current = false;
+        }, 2000);
+    }
+
     function handleLengthChange({ target } : React.ChangeEvent<HTMLInputElement>) {
         setLength(Number(target.value));
     }
@@ -70,7 +108,10 @@ function Generator() {
                 
                 <form>
                     <input readOnly value={password} placeholder='Generate the password...' type="text" />
-                    <button className="copy-btn btn"><img src={copyImg} alt="copyImg" /></button>
+                    <div className="copy-container">
+                        <button onClick={handleCopyButtonClick} className="copy-btn btn"><img src={copyImg} alt="copyImg" /></button>
+                        {showCopyMessage && <div className='copy-message'>{copyMessage}</div>}
+                    </div>
                     <button onClick={generatePassword} className="generate-btn">Generate</button>
                 </form>
             </div>
@@ -82,10 +123,10 @@ function Generator() {
                     <input value={length} onChange={handleLengthChange} placeholder='Enter the length...' />
 
                     <div className="condition-buttons">
-                        <ConditionButton active={includeUppercase} onClick={() => handleButtonClick('uppercase')}>ABC</ConditionButton>
-                        <ConditionButton active={includeLowercase} onClick={() => handleButtonClick('lowercase')}>abc</ConditionButton>
-                        <ConditionButton active={includeNumbers} onClick={() => handleButtonClick('numbers')}>123</ConditionButton>
-                        <ConditionButton active={includeSpecial} onClick={() => handleButtonClick('special')}>!#@</ConditionButton>
+                        <ConditionButton active={includeUppercase} onClick={() => handleConditionButtonClick('uppercase')}>ABC</ConditionButton>
+                        <ConditionButton active={includeLowercase} onClick={() => handleConditionButtonClick('lowercase')}>abc</ConditionButton>
+                        <ConditionButton active={includeNumbers} onClick={() => handleConditionButtonClick('numbers')}>123</ConditionButton>
+                        <ConditionButton active={includeSpecial} onClick={() => handleConditionButtonClick('special')}>!#@</ConditionButton>
                     </div>
                 </div>
             </div>
